993e21223069b5b8a05cef90e3f7b3d555644d96
Marco Tulio <marcotulionogueira@gmail.com>
2023-11-27 21:45:34 -0300

    commit

--- projeto_padaria/projeto_padaria/settings.py
+++ projeto_padaria/projeto_padaria/settings.py
@@ -117,14 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# static e static_url que manipulará os arquivos css, js, etc.
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+# # static e static_url que manipulará os arquivos css, js, etc.
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+# STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
